86fe507e040392c9c8dc93aed234b374e379e1a6
vishwa3 <vishwanathkrishnappa@gmail.com>
2024-10-23 18:55:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useMemo, useCallback } from "react";
+import "./App.css";
 
-function App() {
+// https://jsonplaceholder.typicode.com/users
+
+export default function App() {
+  const [users, setUsers] = useState([]);
+  const [sortOrder, setSortOrder] = useState(true);
+  const [sortBy, setSortBy] = useState("name");
+
+  useEffect(() => {
+    async function getUsers() {
+      const users = await fetch("https://jsonplaceholder.typicode.com/users");
+      const jsonUsers = await users.json();
+      setUsers(jsonUsers);
+    }
+    getUsers();
+  }, []);
+
+  /* function sortNames(sortBy, sortOrder) {
+    let sortedArray;
+    if (sortOrder)
+      sortedArray = [...users].sort(
+        (a, b) => (a[sortBy] > b[sortBy]) - (a[sortBy] < b[sortBy])
+      );
+    else
+      sortedArray = [...users].sort(
+        (a, b) => (a[sortBy] < b[sortBy]) - (a[[sortBy]] > b[sortBy])
+      );
+    return sortedArray;
+  } */
+
+  const sortNames = useCallback(
+    (sortBy, sortOrder) => {
+      let sortedArray;
+      if (sortOrder)
+        sortedArray = [...users].sort(
+          (a, b) => (a[sortBy] > b[sortBy]) - (a[sortBy] < b[sortBy])
+        );
+      else
+        sortedArray = [...users].sort(
+          (a, b) => (a[sortBy] < b[sortBy]) - (a[[sortBy]] > b[sortBy])
+        );
+      return sortedArray;
+    },
+    [users]
+  );
+
+  const sortedArray = useMemo(() => {
+    return sortNames(sortBy, sortOrder);
+  }, [sortBy, sortOrder, sortNames]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => setSortOrder((sortOrder) => !sortOrder)}>
+        Sort <span>{sortOrder ? "Descending" : "Ascending"}</span>
+      </button>
+      <button onClick={() => setSortBy("name")} style={{ marginLeft: "5px" }}>
+        Sort by name
+      </button>
+      <button onClick={() => setSortBy("email")} style={{ marginLeft: "5px" }}>
+        Sort by email
+      </button>
+      {sortedArray.map((user) => {
+        return (
+          <ul key={user.id}>
+            <li>{user.name}</li>
+            <li>{user.email}</li>
+            <li>{`${user.address.suite}, ${user.address.street}, ${user.address.city}, ${user.address.zipcode}`}</li>
+          </ul>
+        );
+      })}
     </div>
   );
 }
-
-export default App;
